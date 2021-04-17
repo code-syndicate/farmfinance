f4a6f98452bf376d206245f54578dc8fc0b14f6a
Timmy <70575836+timmypelumy@users.noreply.github.com>
2021-04-17 14:56:24 +0100

    fresh updates

---
+++ farmfinancebot/credentials.py
@@ -0,0 +1,3 @@
+# Do not remove these 2 lines:
+BOT_TOKEN = '1754440258:AAHLldYtMbsiW-BT9vz1YDDrS14ZG3iHLfo'
+APP_NAME = 'farmfinancebot'
--- main/urls.py
+++ main/urls.py
@@ -2,13 +2,15 @@ from django.urls import path,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from farmfinancebot import urls as farmfinancebot_urls
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', include('django_telegrambot.urls')),
+
     path('', views.IndexView, name = 'indexview'),
     path('tokenomics/', views.TokenomicsView, name = 'tokenomics'),
+    path('farmfinancebot/', include(farmfinancebot_urls))
     
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
