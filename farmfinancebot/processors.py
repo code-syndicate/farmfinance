e3eee29ea62fb9fc5db82ca8d12880e169b9c85d
Timmy <70575836+timmypelumy@users.noreply.github.com>
2021-04-17 16:50:58 +0100

    fresh updates

--- farmfinancebot/processors.py
+++ farmfinancebot/processors.py
@@ -1,5 +1,6 @@
 from django_tgbot.decorators import processor
 from django_tgbot.state_manager import message_types, update_types, state_types
+from django_tgbot.exceptions import ProcessFailure
 from django_tgbot.types.update import Update
 from .bot import state_manager
 from .models import TelegramState
@@ -19,3 +20,24 @@ from .bot import TelegramBot
 def begin( bot : TelegramBot, update : Update, state : TelegramState):
     msg = "Hello!, welcome to Farm Finance.Click /start to get started"
     bot.sendMessage( update.get_chat().get_id(), msg  )
+
+
+# Start Command acceptor
+@processor(
+    state_manager,
+    from_states = 'waiting_for_start_command',
+    update_types = [ update_types.EditedMessage, update_types.Message],
+    message_types = message_types.Text,
+    success = 'started_bot',
+    fail = state_types.Keep,
+)
+def start(bot : TelegramBot, update : Update, state : TelegramState):
+    chat_msg = state.get_message().get_text()
+    if chat_msg == '/start':
+        msg = "Welcome to FarmFinance,follow these /procedures to qualify for our airdrop.Send /commands for a list of available commands."
+        bot.sendMessage( update.get_chat().get_id(), msg )
+    else:
+        msg = 'Please send a valid command.Check /commands'
+        bot.sendMessage( update.get_chat().get_id(), msg)
+        raise ProcessFailure
+
