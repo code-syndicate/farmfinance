5fe1f373b07c43b5d4977fce171f8798dec2446b
Timmy <70575836+timmypelumy@users.noreply.github.com>
2021-04-17 15:19:58 +0100

    fresh updates

--- farmfinancebot/processors.py
+++ farmfinancebot/processors.py
@@ -10,4 +10,4 @@ from .bot import TelegramBot
 def hello_world(bot: TelegramBot, update: Update, state: TelegramState):
     msg = "Hello, welcome to Farm Finance. Please complete the necessary conditions " + 
     "to qualify for our upcoming airdrop. Send /info for more info"
-    bot.sendMessage(update.get_chat().get_id(), 'Hello!')
+    bot.sendMessage(update.get_chat().get_id(), msg )
